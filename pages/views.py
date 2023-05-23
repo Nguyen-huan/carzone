355f500408ba2f157a6226fa769bfc63626fdc99
Nguyen-huan <huan06949@gmail.com>
2023-05-24 01:22:07 +0700

    created pages and configured navigation link

--- pages/views.py
+++ pages/views.py
@@ -10,3 +10,15 @@ def home(request):
     print("file cha 1", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
     return render(request, "pages/home.html", {})
+
+
+def contact(request):
+    return render(request, "pages/contact.html")
+
+
+def about(request):
+    return render(request, "pages/about.html")
+
+
+def services(request):
+    return render(request, "pages/services.html")
