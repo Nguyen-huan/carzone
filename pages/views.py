d32ad621146342a2625564ec155aa8ea5552d709
Nguyen-huan <huan06949@gmail.com>
2023-05-24 17:57:13 +0700

    Complete Team Model

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from .models import Team
 import os
 
 
 # Create your views here.
 def home(request):
-    print("file tuong doi", __file__)
-    print("file tuyet doi", os.path.abspath(__file__))
-    print("file cha 1", os.path.dirname(os.path.abspath(__file__)))
-    print("file cha 1", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-    return render(request, "pages/home.html", {})
+    # print("file tuong doi", __file__)
+    # print("file tuyet doi", os.path.abspath(__file__))
+    # print("file cha 1", os.path.dirname(os.path.abspath(__file__)))
+    # print("file cha 1", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, "pages/home.html", data)
 
 
 def contact(request):
@@ -17,7 +21,11 @@ def contact(request):
 
 
 def about(request):
-    return render(request, "pages/about.html")
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, "pages/about.html", data)
 
 
 def services(request):
